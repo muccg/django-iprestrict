bb3d9b71c4d27dcae9e6a669d2bff3c60577b545
Tamas Szabo <szabtam@gmail.com>
2012-07-03 23:30:54 +0800

    Moves IPRestrictMiddleware right after CommonMiddleware.

--- iprestrict/test_settings.py
+++ iprestrict/test_settings.py
@@ -94,13 +94,13 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'iprestrict.middleware.IPRestrictMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'iprestrict.middleware.IPRestrictMiddleware'
 )
 
 ROOT_URLCONF = 'iprestrict.urls'
