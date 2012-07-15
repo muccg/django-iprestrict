daabf8cc560e003d9c93fe6255e82eed094cb005
Tamas Szabo <szabtam@gmail.com>
2012-07-15 17:24:19 +0800

    Adds setup.py file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-iprestrict',
+    version='0.1',
+    description='Django app + middleware to restrict access to all or sections of a Django project by client IP ranges',
+    long_description='Django app + middleware to restrict access to all or sections of a Django project by client IP ranges',
+    author='Tamas Szabo',
+    url='https://github.com/sztamas/django-iprestrict',
+    download_url='https://github.com/sztamas/django-iprestrict/downloads',
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    packages=[
+        'iprestrict',
+    ],
+)
