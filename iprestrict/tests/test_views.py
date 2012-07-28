3c444cf451c7f1459a0a5080d3cbb2fda5ca8df9
Tamas Szabo <szabtam@gmail.com>
2012-07-28 13:37:14 +0800

    Adds default allow rule for locahost. Changes tests accordingly and at tests for reload view.

---
+++ iprestrict/tests/test_views.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from iprestrict import models
+from iprestrict import restrictor
+
+class ReloadViewTest(TestCase):
+    IP = '192.168.1.1'
+
+    def add_allow_rule(self):
+        localip = models.IPGroup.objects.create(name='Local IP')
+        models.IPRange.objects.create(ip_group=localip, first_ip=self.IP)
+        models.Rule.objects.create(url_pattern='ALL', ip_group = localip, action='A')
+
+    def test_reload_view(self):
+        # This test doesn't conform to unit test best practices.
+        # Unfortunately, it does depend on order so it is in one method
+        # instead of 3 methods, each asserting one thing
+
+        # 1
+        response = self.client.get('', REMOTE_ADDR = self.IP)
+        self.assertEqual(response.status_code, 403, 'Should be restricted')
+
+        # 2
+        self.add_allow_rule()
+        response = self.client.get('', REMOTE_ADDR = self.IP)
+        self.assertEqual(response.status_code, 403, 'Should still be restricted - rules have not been reloaded')
+
+        # 3 reload rules
+        response = self.client.get('/iprestrict/reload_rules')
+        response = self.client.get('', REMOTE_ADDR = self.IP)
+        self.assertEqual(response.status_code, 404, 'Should be allowed now')
