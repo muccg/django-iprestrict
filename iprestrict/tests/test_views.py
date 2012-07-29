633f6ce2ed93edb994ef0d5053ac7123984bd1ba
Tamas Szabo <szabtam@gmail.com>
2012-07-29 08:07:41 +0800

    Re-enables test_reload_view() (login with superuser).

--- iprestrict/tests/test_views.py
+++ iprestrict/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils.unittest import skip
-
+from django.contrib.auth.models import User
 
 from iprestrict import models
 from iprestrict import restrictor
@@ -8,12 +8,17 @@ from iprestrict import restrictor
 class ReloadViewTest(TestCase):
     IP = '192.168.1.1'
 
+    def setUp(self):
+        admin = User.objects.create_user('admin', 'admin@nohost.org', 'pass')
+        admin.is_staff = True
+        admin.is_superuser = True
+        admin.save()
+
     def add_allow_rule(self):
         localip = models.IPGroup.objects.create(name='Local IP')
         models.IPRange.objects.create(ip_group=localip, first_ip=self.IP)
         models.Rule.objects.create(url_pattern='ALL', ip_group = localip, action='A')
 
-    @skip('Have to log in to test reload rules view')
     def test_reload_view(self):
         # This test doesn't conform to unit test best practices.
         # Unfortunately, it does depend on order so it is in one method
@@ -30,6 +35,7 @@ class ReloadViewTest(TestCase):
 
         # 3 reload rules
         # TODO I have to log in as superuser for this to work
+        self.client.login(username='admin', password='pass')
         response = self.client.get('/iprestrict/reload_rules')
         response = self.client.get('', REMOTE_ADDR = self.IP)
         self.assertEqual(response.status_code, 404, 'Should be allowed now')
