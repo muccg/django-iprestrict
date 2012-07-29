a2f42923e17df41003bf20bdf2877d7a8d0b4994
Tamas Szabo <szabtam@gmail.com>
2012-07-29 08:47:56 +0800

    Adds test for custom command.

--- iprestrict/management/commands/reload_rules.py
+++ iprestrict/management/commands/reload_rules.py
@@ -5,5 +5,8 @@ class Command(BaseCommand):
     help = 'Reloads the ip restriction rules from the DB'
 
     def handle(self, *args, **options):
+        verbosity = int(options.get('verbosity', '1'))
+        
         models.ReloadRulesRequest.request_reload()
-        self.stdout.write('Successfully reloaded rules\n')
+        if verbosity >= 1:
+            self.stdout.write('Successfully reloaded rules\n')
--- iprestrict/tests/test_middleware.py
+++ iprestrict/tests/test_middleware.py
@@ -31,11 +31,15 @@ class MiddlewareRestrictsTest(TestCase):
         self.assert_ip_is_restricted('10.10.10.1')
         self.assert_ip_is_restricted('169.254.0.1')
 
+LOCAL_IP = '192.168.1.1'
+def create_ip_allow_rule(ip=LOCAL_IP):
+    localip = models.IPGroup.objects.create(name='localip')
+    models.IPRange.objects.create(ip_group=localip, first_ip='192.168.1.1')
+    models.Rule.objects.create(url_pattern='ALL', ip_group = localip, action='A')
+
 class MiddlewareAllowsTest(TestCase):
     def setUp(self):
-        localip = models.IPGroup.objects.create(name='localip')
-        models.IPRange.objects.create(ip_group=localip, first_ip='192.168.1.1')
-        models.Rule.objects.create(url_pattern='ALL', ip_group = localip, action='A')
+        create_ip_allow_rule()
         models.ReloadRulesRequest.request_reload()
 
     def test_middleware_allows_localhost(self):
@@ -43,10 +47,22 @@ class MiddlewareAllowsTest(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_middleware_allows_ip_just_added(self):
-        response = self.client.get('', REMOTE_ADDR = '192.168.1.1')
+        response = self.client.get('', REMOTE_ADDR = LOCAL_IP)
         self.assertEqual(response.status_code, 404)
 
     def test_middleware_restricts_other_ip(self):
         response = self.client.get('', REMOTE_ADDR = '10.1.1.1')
         self.assertEqual(response.status_code, 403)
 
+class ReloadRulesTest(TestCase):
+    def setUp(self):
+        create_ip_allow_rule()
+
+    def test_reload_with_custom_command(self):
+        # Update 
+        from django.core.management import call_command
+        call_command('reload_rules', verbosity=0)
+
+        response = self.client.get('', REMOTE_ADDR = LOCAL_IP)
+        self.assertEqual(response.status_code, 404)
+   
