19e0814b1da398a5ba4216e64740c647d21da313
Tamas Szabo <szabtam@gmail.com>
2012-10-02 09:49:12 +0800

    Adds management command to import(replace) rules from fixtures.

--- iprestrict/management/commands/reloadrules.py
+++ iprestrict/management/commands/reloadrules.py
@@ -2,11 +2,11 @@ from django.core.management.base import BaseCommand
 from iprestrict import models
 
 class Command(BaseCommand):
-    help = 'Reloads the ip restriction rules from the DB'
+    help = 'Requests the reload of the ip restriction rules from the DB.'
 
     def handle(self, *args, **options):
         verbosity = int(options.get('verbosity', '1'))
         
         models.ReloadRulesRequest.request_reload()
         if verbosity >= 1:
-            self.stdout.write('Successfully reloaded rules\n')
+            self.stdout.write('Successfully requested reload of rules\n')
