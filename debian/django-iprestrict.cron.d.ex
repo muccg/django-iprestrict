3fedcfaf35b42d8b0940549c830ee053b0a1c4e3
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2014-01-02 09:41:41 +0800

    Initial debian package

--- debian/django-iprestrict.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the django-iprestrict package
-#
-0 4	* * *	root	[ -x /usr/bin/django-iprestrict_maintenance ] && /usr/bin/django-iprestrict_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(django-iprestrict):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="django-iprestrict" command="/usr/bin/django-iprestrict"
