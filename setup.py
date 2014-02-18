bd8e026f46af9003cacbfb080a4fff88a1889a5f
Dmitry <admin@topnomer.ru>
2014-02-18 21:30:14 +0400

    support for other django versions

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         ]
     },
     install_requires=[
-        'South==0.7.6',
-        'django-templatetag-handlebars==1.2.0',
+        'South>=0.7.6',
+        'django-templatetag-handlebars>=1.2.0',
     ]
 )
