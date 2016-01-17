3c288d0306f7952719adc063c65aa60aa8b93cd5
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-01-17 12:12:04 +0000

    middleware: Small refactor in extract_client_ip()

--- iprestrict/middleware.py
+++ iprestrict/middleware.py
@@ -35,9 +35,8 @@ class IPRestrictMiddleware(object):
     def extract_client_ip(self, request):
         client_ip = request.META['REMOTE_ADDR']
         if not self.ignore_proxy_header:
-            forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-            if forwarded_for is not None:
-                forwarded_for = [ip.strip() for ip in forwarded_for.split(',')]
+            forwarded_for = self.get_forwarded_for(request)
+            if forwarded_for:
                 closest_proxy = client_ip
                 client_ip = forwarded_for.pop(0)
                 proxies = [closest_proxy] + forwarded_for
@@ -46,6 +45,13 @@ class IPRestrictMiddleware(object):
                         logger.info("Client IP %s forwarded by untrusted proxy %s" % (client_ip, proxy))
                         raise exceptions.PermissionDenied
         return client_ip
+
+    def get_forwarded_for(self, request):
+        hdr = request.META.get('HTTP_X_FORWARDED_FOR')
+        if hdr is not None:
+            return [ip.strip() for ip in hdr.split(',')]
+        else:
+            return []
 
     def reload_rules_if_needed(self):
         last_reload_request = ReloadRulesRequest.last_request()
