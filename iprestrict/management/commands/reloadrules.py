c490f325c56ca3dba60203b93643e62f798cc76d
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-07-07 12:28:37 +0300

    Issue #14: Show error message on reloadrules if IPRESTRICT_RELOAD_RULES is False

--- iprestrict/management/commands/reloadrules.py
+++ iprestrict/management/commands/reloadrules.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from ... import models
+from ...middleware import get_reload_rules_setting
 
 
 class Command(BaseCommand):
@@ -7,6 +8,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         verbosity = int(options.get('verbosity', '1'))
+
+        reload_rules = get_reload_rules_setting()
+        if not reload_rules:
+            self.stderr.write("IPRESTRICT_RELOAD_RULES is set to False. "
+                    "Your IPRestrict rules can't be changed dynamically.")
+            return
 
         models.ReloadRulesRequest.request_reload()
         if verbosity >= 1:
