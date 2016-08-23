34ee4fc426180f9a73c2bfec0f4c1b112d476a28
Tamas Szabo <szabtam@gmail.com>
2016-08-23 20:23:13 +0300

    Issue #15: NoReverseMatch error with Django 1.9

--- iprestrict/urls.py
+++ iprestrict/urls.py
@@ -5,8 +5,8 @@ from .views import move_rule_up, move_rule_down
 app_name = "iprestrict"
 urlpatterns = [
     url(r'^$', test_rules_page),
-    url(r'^move_rule_up/(?P<rule_id>\d+)[/]?$', move_rule_up),
-    url(r'^move_rule_down/(?P<rule_id>\d+)[/]?$', move_rule_down),
-    url(r'^reload_rules[/]?$', reload_rules),
-    url(r'^test_match[/]?$', test_match),
+    url(r'^move_rule_up/(?P<rule_id>\d+)[/]?$', move_rule_up, name='move_rule_up'),
+    url(r'^move_rule_down/(?P<rule_id>\d+)[/]?$', move_rule_down, name='move_rule_down'),
+    url(r'^reload_rules[/]?$', reload_rules, name='reload_rules'),
+    url(r'^test_match[/]?$', test_match, name='test_match'),
 ]
