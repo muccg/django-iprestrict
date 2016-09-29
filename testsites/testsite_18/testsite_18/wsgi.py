bf5ca8cdd522addb23a45f0eeefc50b5a1f0e275
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-09-29 14:20:48 +0300

    Added testsites for testing under different versions of Django.

---
+++ testsites/testsite_18/testsite_18/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testsite_18 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite_18.settings")
+
+application = get_wsgi_application()
