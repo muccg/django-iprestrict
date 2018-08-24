2915d30259ab88a5b66144c421c6c53686ed258b
Jon Torodash <jon@vetcove.com>
2018-08-24 17:26:24 -0400

    Django 2.1 compatibility

--- iprestrict/decorators.py
+++ iprestrict/decorators.py
@@ -6,7 +6,12 @@ import inspect
 
 from django.utils.translation import ugettext as _
 from django.contrib.admin.forms import AdminAuthenticationForm
-from django.contrib.auth.views import login
+
+try:
+    from django.contrib.auth.views import login
+except ImportError:
+    from django.contrib.auth import login
+
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.http import HttpResponseForbidden
 
