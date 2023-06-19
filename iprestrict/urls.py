98912bd1f0e24b54aaf16d1faacfb1962a6c3516
milad <milad_khaleghi@live.com>
2023-06-19 20:12:05 +0330

    FIX(url): fix incompatibility with django 4 replace deprecated function url with re_path

--- iprestrict/urls.py
+++ iprestrict/urls.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 from .views import test_rules_page, test_match, reload_rules
 from .views import move_rule_up, move_rule_down
 
 app_name = "iprestrict"
 urlpatterns = [
-    url(r'^$', test_rules_page),
-    url(r'^move_rule_up/(?P<rule_id>\d+)[/]?$', move_rule_up, name='move_rule_up'),
-    url(r'^move_rule_down/(?P<rule_id>\d+)[/]?$', move_rule_down, name='move_rule_down'),
-    url(r'^reload_rules[/]?$', reload_rules, name='reload_rules'),
-    url(r'^test_match[/]?$', test_match, name='test_match'),
+    re_path(r'^$', test_rules_page),
+    re_path(r'^move_rule_up/(?P<rule_id>\d+)[/]?$', move_rule_up, name='move_rule_up'),
+    re_path(r'^move_rule_down/(?P<rule_id>\d+)[/]?$', move_rule_down, name='move_rule_down'),
+    re_path(r'^reload_rules[/]?$', reload_rules, name='reload_rules'),
+    re_path(r'^test_match[/]?$', test_match, name='test_match'),
 ]
